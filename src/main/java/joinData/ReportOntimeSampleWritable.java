74580ebe1c02001ffa7b8825eaa3074d977ead74
Ilya Karkin <karkinia@yandex.ru>
2020-10-09 21:20:31 +0300

    Scripted auto-commit on change (2020-10-09 21:20:31) by gitwatch.sh

--- src/main/java/joinData/ReportOntimeSampleWritable.java
+++ src/main/java/joinData/ReportOntimeSampleWritable.java
@@ -1,4 +1,25 @@
 package joinData;
 
-public class ReportOntimeSampleWritable {
+import org.apache.hadoop.io.Writable;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+public class ReportOntimeSampleWritable implements Writable {
+
+    private int code;
+    private String description;
+
+    @Override
+    public void write(DataOutput out) throws IOException {
+        out.writeInt(code);
+        out.writeUTF(description);
+    }
+
+    @Override
+    public void readFields(DataInput in) throws  IOException {
+        code = in.readInt();
+        description = in.readUTF();
+    }
 }
