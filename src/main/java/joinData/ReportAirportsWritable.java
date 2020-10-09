4aae444a387803052ed45c8b5cd64503882d2c02
Ilya Karkin <karkinia@yandex.ru>
2020-10-09 21:16:55 +0300

    Scripted auto-commit on change (2020-10-09 21:16:55) by gitwatch.sh

--- src/main/java/joinData/ReportAirportsWritable.java
+++ src/main/java/joinData/ReportAirportsWritable.java
@@ -1,7 +1,22 @@
 import org.apache.hadoop.io.Writable;
 
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
 public class ReportAirportsWritable implements Writable {
 
-    privale
+    private int code;
+    private String description;
+
+
+    public void write(DataOutput out) throws IOException {
+        out.writeInt(code);
+        out.writeUTF(description);
+    }
+
+    public void readFields(DataInput in) throws  IOException {
+
+    }
 
 }
