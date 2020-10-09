99a4c917c507ec7d1647ae5723fb969b5c27a562
Ilya Karkin <karkinia@yandex.ru>
2020-10-09 22:39:08 +0300

    Scripted auto-commit on change (2020-10-09 22:39:08) by gitwatch.sh

--- src/main/java/joinData/ReportApp.java
+++ src/main/java/joinData/ReportApp.java
@@ -15,9 +15,17 @@ public class ReportApp {
     public static void main(String[] args) throws Exception {
         Job job = Job.getInstance();
         job.setJarByClass(ReportApp.class);
-        job.setJobName("Report job");
-        MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, ReportMapper.class);
-        MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, SystemsJoinMapper.class);
-
+        job.setJobName("JoinJob sort");
+        MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, CallsJoinMapper.class);
+        MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, SystemsJoinMapper.class);
+        FileOutputFormat.setOutputPath(job, new Path(args[2]));
+        job.setPartitionerClass(TextPair.FirstPartitioner.class);
+        job.setGroupingComparatorClass(TextPair.FirstComparator.class);
+        job.setReducerClass(JoinReducer.class);
+        job.setMapOutputKeyClass(TextPair.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(Text.class);
+        job.setNumReduceTasks(2);
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
