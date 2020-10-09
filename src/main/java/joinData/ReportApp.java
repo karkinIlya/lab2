e5f5cd733b5252b237b8f58d7e09630670aaaadb
Ilya Karkin <karkinia@yandex.ru>
2020-10-09 22:36:39 +0300

    Scripted auto-commit on change (2020-10-09 22:36:39) by gitwatch.sh

--- src/main/java/joinData/ReportApp.java
+++ src/main/java/joinData/ReportApp.java
@@ -5,27 +5,19 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapred.FileOutputFormat;
 import org.apache.hadoop.mapred.JobClient;
 import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapred.TextInputFormat;
 import org.apache.hadoop.mapred.join.CompositeInputFormat;
+import org.apache.hadoop.mapred.lib.MultipleInputs;
+import org.apache.hadoop.mapreduce.Job;
 
 
 public class ReportApp {
     public static void main(String[] args) throws Exception {
-        if (args.length != 2) {
-            System.err.println("Usage: HadoopMapReduce.WordCountApp <input path> <output path>");
-            System.exit(-1);
-        }
-        JobConf conf = new JobConf(ReportApp.class);
-        conf.setJobName("map join");
-        conf.setInputFormat(CompositeInputFormat.class);
-        FileOutputFormat.setOutputPath(conf, new Path(args[2]));
-        conf.set("mapred.join.expr", CompositeInputFormat.compose("inner",
-                KeyValueTextInputFormat.class,
-                args[0],
-                args[1]
-        ));
-        conf.setMapperClass(ReportMapper.class);
-        conf.setOutputKeyClass(Text.class);
-        conf.setOutputValueClass(Text.class);
-        JobClient.runJob(conf);
+        Job job = Job.getInstance();
+        job.setJarByClass(ReportApp.class);
+        job.setJobName("Report job");
+        MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, ReportMapper.class);
+        MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, SystemsJoinMapper.class);
+
     }
 }
