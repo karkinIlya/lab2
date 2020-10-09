65100b260a06e00e3c8e7d6ac1bd7eeb314bbb64
Ilya Karkin <karkinia@yandex.ru>
2020-10-09 22:15:01 +0300

    Scripted auto-commit on change (2020-10-09 22:15:01) by gitwatch.sh

---
+++ src/main/java/joinData/ReportMapper.java
@@ -0,0 +1,16 @@
+package joinData;
+
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.MapReduceBase;
+import org.apache.hadoop.mapred.Mapper;
+import org.apache.hadoop.mapred.join.TupleWritable;
+
+import java.io.IOException;
+
+public class ReportMapper extends MapReduceBase implements Mapper<Text, TupleWritable, Text, Text> {
+    @Override
+    public void map(Text key, TupleWritable value, OutputCollector<Text, Text> output,
+                    Report reporter) throws IOException {
+
+    }
+}
