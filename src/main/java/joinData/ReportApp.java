8c7f9a740584f85ffab76b374463850b05325c7a
Ilya Karkin <karkinia@yandex.ru>
2020-10-09 20:15:21 +0300

    Scripted auto-commit on change (2020-10-09 20:15:21) by gitwatch.sh

--- src/main/java/joinData/ReportApp.java
+++ src/main/java/joinData/ReportApp.java
@@ -1,5 +1,10 @@
 public class RaportApp {
     public static void main(String[] args) throws Exception {
-
+        f (args.length != 2) {
+            System.err.println("Usage: HadoopMapReduce.WordCountApp <input path> <output path>");
+            System.exit(-1);
+        }
+        Job job = Job.getInstance();
+        job.setJarByClass(ReportApp.class);
     }
 }
