959c1200fa6ca59f707276fd53f7c2337393d00b
Ilya Karkin <karkinia@yandex.ru>
2020-10-09 20:29:07 +0300

    Scripted auto-commit on change (2020-10-09 20:29:07) by gitwatch.sh

--- src/main/java/joinData/ReportApp.java
+++ src/main/java/joinData/ReportApp.java
@@ -4,6 +4,20 @@ public class RaportApp {
             System.err.println("Usage: HadoopMapReduce.WordCountApp <input path> <output path>");
             System.exit(-1);
         }
+        JobConf conf = new JobConf(JoinJob.class);
+        conf.setJobName("map join");
+        conf.setInputFormat(CompositeInputFormat.class);
+        FileOutputFormat.setOutputPath(conf, new Path(args[2]));
+        conf.set("mapred.join.expr", CompositeInputFormat.compose("inner",
+                KeyValueTextInputFormat.class,
+                args[0],
+                args[1]
+        ));
+        conf.setMapperClass(MapJoinMapper.class);
+        conf.setOutputKeyClass(Text.class);
+        conf.setOutputValueClass(Text.class);
+        JobClient.runJob(conf);
+        
         Job job = Job.getInstance();
         job.setJarByClass(ReportApp.class);
         job.setJobName("Report");
