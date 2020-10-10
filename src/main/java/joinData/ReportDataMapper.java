5bd27ab11e489d6b398517252a746538a684729e
Ilya Karkin <karkinia@yandex.ru>
2020-10-10 17:50:09 +0300

    Scripted auto-commit on change (2020-10-10 17:50:09) by gitwatch.sh

--- src/main/java/joinData/ReportDataMapper.java
+++ src/main/java/joinData/ReportDataMapper.java
@@ -16,7 +16,7 @@ public class ReportDataMapper extends Mapper<LongWritable, Text, Text, Text> {
         String line = value.toString();
         String[] data = line.split(",");
         for (int i = 1; i < data.length; i++) {
-            if (data[19] != "0.00" && data[18] != "0.00") {
+            if (data[19] == "0.00" && data[18] != "0.00") {
 
             }
             context.write(new Text(data[0]), new Text("1"));
