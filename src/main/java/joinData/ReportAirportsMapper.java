67b322dd99ca4c08a37e1ff05d80c939e93a64e0
Ilya Karkin <karkinia@yandex.ru>
2020-10-10 17:42:35 +0300

    Scripted auto-commit on change (2020-10-10 17:42:35) by gitwatch.sh

--- src/main/java/joinData/ReportAirportsMapper.java
+++ src/main/java/joinData/ReportAirportsMapper.java
@@ -9,11 +9,11 @@ import java.io.IOException;
 
 public class ReportAirportsMapper extends Mapper<LongWritable, Text, Text, Text> {
     @Override
-    public void map(LongWritable key, Text value, Context context) throws IOException {
+    public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         String line = value.toString();
         String[] data = line.split(",");
         for (int i = 1; i < data.length; i++) {
-            context.write(new Text(key.toString()), new Text("0"));
+            context.write(new Text(data[0]), new Text("0"));
         }
     }
 }
