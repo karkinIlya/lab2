0d1fd1a0e6326be3e61e34dc840a65f611d4fcbe
Ilya Karkin <karkinia@yandex.ru>
2020-10-11 14:05:36 +0300

    Scripted auto-commit on change (2020-10-11 14:05:36) by gitwatch.sh

--- src/main/java/joinData/GroupingComparator.java
+++ src/main/java/joinData/GroupingComparator.java
@@ -1,7 +1,12 @@
 package joinData;
 
 import org.apache.hadoop.io.RawComparator;
+import org.jetbrains.annotations.NotNull;
 
-public class GroupingComparator implements RawComparator<TextPair> {
+public class GroupingComparator implements Comparable<TextPair> {
 
+    @Override
+    public int compareTo(@NotNull TextPair o) {
+        return 0;
+    }
 }
