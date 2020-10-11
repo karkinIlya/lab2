0c6aad1b6dcb0e0cdd895b318ed9f5c1c46f90d9
Ilya Karkin <karkinia@yandex.ru>
2020-10-11 13:24:10 +0300

    Scripted auto-commit on change (2020-10-11 13:24:10) by gitwatch.sh

--- src/main/java/joinData/ReportAirportsMapper.java
+++ src/main/java/joinData/ReportAirportsMapper.java
@@ -13,7 +13,7 @@ public class ReportAirportsMapper extends Mapper<LongWritable, Text, TextPair, T
         String line = value.toString();
         String[] data = line.split(",");
         for (int i = 0; i < data.length; i++) {
-            if (data[0] != "Code") {
+            if (!data[0].equals("Code")) {
                 String tmp = data[0].replace("\"", "");
                 context.write(new TextPair(tmp, "0"), new Text(tmp));
             }
