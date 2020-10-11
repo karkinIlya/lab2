d17f33d2b201cdd7864f3e24115335bc4c19a9b7
Ilya Karkin <karkinia@yandex.ru>
2020-10-11 14:20:44 +0300

    Scripted auto-commit on change (2020-10-11 14:20:44) by gitwatch.sh

--- src/main/java/joinData/GroupingComparator.java
+++ src/main/java/joinData/GroupingComparator.java
@@ -1,10 +1,17 @@
 package joinData;
 
-import org.apache.hadoop.io.RawComparator;
+import org.apache.hadoop.io.WritableComparable;
 import org.apache.hadoop.io.WritableComparator;
 
-public class GroupingComparator extends WritableComparator  {
-    public int compare (TextPair o1, TextPair o2) {
-        return o1.getFirst().compareTo(o2.getFirst());
+public class GroupingComparator extends WritableComparator {
+    public GroupingComparator() {
+        super(TextPair.class, true);
+    }
+
+    @Override
+    public int compare (WritableComparable o1, WritableComparable o2) {
+        final TextPair textPair1 = (TextPair)o1;
+        final TextPair textPair2 = (TextPair)o2;
+        return textPair1.getFirst().compareTo(textPair2.getFirst());
     }
 }
