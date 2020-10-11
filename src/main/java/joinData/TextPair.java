63a71c7bc7fac5d4493b59f1d748d2c6677369ce
Ilya Karkin <karkinia@yandex.ru>
2020-10-11 11:55:11 +0300

    Scripted auto-commit on change (2020-10-11 11:55:11) by gitwatch.sh

--- src/main/java/joinData/TextPair.java
+++ src/main/java/joinData/TextPair.java
@@ -3,6 +3,10 @@ package joinData;
 import org.apache.hadoop.io.WritableComparable;
 import org.jetbrains.annotations.NotNull;
 
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
 public class TextPair implements WritableComparable {
     public TextPair(String first, String second) {
         this.first = first;
@@ -21,7 +25,19 @@ public class TextPair implements WritableComparable {
     }
 
     @Override
-    public int compareTo(@NotNull Object o) {
-        return 0;
+    public int compareTo(@NotNull TextPair o) {
+        int res = first.compareTo(o.getSecond());
+        return res == 0 ? second.compareTo(o.getSecond()) : res;
+    }
+
+    @Override
+    public void write(DataOutput dataOutput) throws IOException {
+        dataOutput.writeUTF(first);
+        dataOutput.writeUTF(second);
+    }
+
+    @Override
+    public void readFields(DataInput dataInput) throws IOException {
+
     }
 }
