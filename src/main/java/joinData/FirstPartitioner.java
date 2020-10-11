09d1a6ab309180bddf675e077ecd8587d83d3e55
Ilya Karkin <karkinia@yandex.ru>
2020-10-11 15:05:45 +0300

    Scripted auto-commit on change (2020-10-11 15:05:45) by gitwatch.sh

--- src/main/java/joinData/FirstPartitioner.java
+++ src/main/java/joinData/FirstPartitioner.java
@@ -3,4 +3,11 @@ package joinData;
 import org.apache.hadoop.mapreduce.Partitioner;
 
 public class FirstPartitioner extends Partitioner {
+    public FirstPartitioner() {
+    }
+
+    @Override
+    public int getPartition(Object o, Object o2, int i) {
+        return 0;
+    }
 }
