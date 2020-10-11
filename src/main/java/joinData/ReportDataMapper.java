75fc9f19252c91c15b36d604cef02ce20b22116d
Ilya Karkin <karkinia@yandex.ru>
2020-10-11 15:29:50 +0300

    Scripted auto-commit on change (2020-10-11 15:29:50) by gitwatch.sh

--- src/main/java/joinData/ReportDataMapper.java
+++ src/main/java/joinData/ReportDataMapper.java
@@ -15,7 +15,7 @@ public class ReportDataMapper extends Mapper<LongWritable, Text, TextPair, Text>
     public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         String line = value.toString();
         String[] data = line.split(",");
-        if (data[19].equals("0.00") && !data[18].equals("0.00") && !data[18].equals("")) {
+        if (!data[18].equals("0.00") && !data[18].equals("")) {
 //            System.out.println("              map data        " + " " + data[14] + " " + data[18]);
             context.write(new TextPair(data[14], "1"),
                     new Text(data[18].substring(1, data[18].length() - 1)));
